ad71547751d1917ab0671824f705fbd47faadf29
ToeToeAung <toetoeag@gmail.com>
2024-09-05 09:30:59 -0500

    Added exercise5.js and exercise6.js

---
+++ Assignment3/exercise5.js
@@ -0,0 +1,18 @@
+/*Exercise 5: What’s the output in the console? And Draw the execution context as we did in the class. Take a photo or screenshot of what you draw, then upload on github. */
+
+
+function b() {
+
+       const  y  =  30; a();
+
+function a() { console.log(x,  y);
+
+}
+
+var  x  =  10; console.log(x, y);
+
+}
+
+const  x  =  20; var  y  =  40; b();
+
+ 
--- Assignment3/exercise6.js
+++ Assignment3/exercise6.js
@@ -1,2 +1,31 @@
-console.log("Test")
-//Test
+/*Exercise 6: What’s the output in the console? And Draw the execution context as we did in the class. Take a photo or screenshot of what you draw, then upload on github. */
+
+
+function  foo(x)  {
+
+         let m;
+
+console.log(x, y);
+
+if(x > 5){
+
+var  y  =  5; m = x + y;
+
+} else {
+
+let  z  =  10; m = z;
+
+}
+
+x = m;
+
+console.log(x, y);
+
+}
+
+
+var  x  =  10;
+
+foo(3);
+
+console.log(x);
