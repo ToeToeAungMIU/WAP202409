fa36dc133b71c53d6a924f861933b72658eb38e5
ToeToeAungMIU <taung@miu.edu>
2024-09-06 22:16:28 -0500

    Delete exercise3.js

--- exercise3.js
+++
@@ -1,5 +0,0 @@
-function describePerson(person) {
-    return "".concat(person.name, " is ").concat(person.age, " years old and is ").concat(person.isStudent ? "a student " : "not a student", ".");
-}
-var person = { name: "Alice", age: 25, isStudent: true };
-console.log(describePerson(person));
